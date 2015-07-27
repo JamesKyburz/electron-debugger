62e31f378515b94e54caea5c5d54cedd678ae724
James Kyburz <james.kyburz@gmail.com>
2015-07-27 16:50:00 +0200

    allow omitting entry point

--- cli.js
+++ cli.js
@@ -6,5 +6,5 @@ var path = require('path')
 
 app.on('ready', function () {
   var mainWindow = new BrowserWindow({show: true})
-  mainWindow.loadUrl('file://' + path.join(__dirname, 'index.html') + '?file=' + process.argv[2])
+  mainWindow.loadUrl('file://' + path.join(__dirname, 'index.html') + '?file=' + (process.argv[2] || ''))
 })
