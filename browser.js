f3af5871b442e628c139773665c111a5c8d79f5d
James Kyburz <james.kyburz@gmail.com>
2016-05-17 16:32:24 +0200

    allow older versions of electron

--- browser.js
+++ browser.js
@@ -1,5 +1,11 @@
 window.onload = function load () {
-  var mainWindow = require('electron').remote.getCurrentWindow()
+  var remote
+  try {
+    remote = require('' + 'electron').remote
+  } catch (e) {
+    remote = require('' + 'remote')
+  }
+  var mainWindow = remote.getCurrentWindow()
   mainWindow.openDevTools()
   var entry = require('querystring').parse(window.location.search.slice(1)).file || '.'
   require(process.cwd() + '/' + entry)
